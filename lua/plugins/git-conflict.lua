8b98f0b991798d835d879c20d0a918096cacc665
Tim0305 <tabtim1015@gmail.com>
2025-03-15 17:47:38 -0600

    Change keymaps

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,3 +1,30 @@
 return {
-	{ "akinsho/git-conflict.nvim", version = "*", config = true },
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    config = function()
+      require("git-conflict").setup({
+        default_mappings = false, -- enable buffer local mapping created by this plugin
+        default_commands = true, -- disable commands created by this plugin
+        disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+        list_opener = "copen",   -- command or function to open the conflicts list
+        highlights = {           -- They must have background color, otherwise the default color will be used
+          incoming = "DiffAdd",
+          current = "DiffText",
+        },
+      })
+      vim.keymap.set("n", "co", ":GitConflictChooseOurs<CR>")
+      vim.keymap.set("n", "ct", ":GitConflictChooseTheirs<CR>")
+      vim.keymap.set("n", "cb", ":GitConflictChooseBoth<CR>")
+      vim.keymap.set("n", "c0", ":GitConflictChooseNone<CR>")
+      --  Move to the next conflict.
+      vim.keymap.set("n", "nx", ":GitConflictNextConflict<CR>normal! zz", { noremap = true, silent = true })
+
+      -- Move to the previous conflict.
+      vim.keymap.set("n", "px", ":GitConflictPrevConflict<CR>normal! zz", { noremap = true, silent = true })
+
+      -- Get all conflict to quickfix
+      vim.keymap.set("n", "qx", ":GitConflictListQf<CR>", { noremap = true, silent = true })
+    end,
+  },
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,31 +1,35 @@
 -- Neo tree. File explorer
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- icons
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function()
-    -- Evitar que neo tree capture C-b en su buffer y sea de forma global para aplicar mi keymap
-      require("neo-tree").setup({
-        default_component_configs = {
-          mappings = {
-            ["<leader>n"] = "none", -- Desactiva <C-b> dentro del árbol para evitar conflictos
-          },
-        },
-        window = {
-          mappings = {
-            ["<leader>n"] = "none", -- Desactiva <C-b> específicamente en las ventanas de Neo-tree
-          },
-        },
-      })
-      -- Toggle con reveal
-      vim.keymap.set('n', '<leader>n', ':Neotree filesystem toggle reveal left<CR>')-- Mensaje de descripcion
-
-    end
-  }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- icons
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		config = function()
+			-- Evitar que neo tree capture C-b en su buffer y sea de forma global para aplicar mi keymap
+			require("neo-tree").setup({
+				default_component_configs = {
+					mappings = {
+						["<leader>n"] = "none", -- Desactiva <C-b> dentro del árbol para evitar conflictos
+					},
+				},
+				window = {
+					mappings = {
+						["<leader>n"] = "none", -- Desactiva <C-b> específicamente en las ventanas de Neo-tree
+					},
+				},
+			})
+			-- Toggle con reveal
+			vim.keymap.set(
+				"n",
+				"<leader>n",
+				":Neotree filesystem toggle reveal left<CR>",
+				{ noremap = true, silent = true }
+			) -- Mensaje de descripcion
+		end,
+	},
 }
