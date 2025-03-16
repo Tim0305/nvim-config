2a878f81400532ed20fac28bd40f0bf4c446d926
Tim0305 <tabtim1015@gmail.com>
2025-03-15 18:10:47 -0600

    Use the enter keymapping

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -76,7 +76,7 @@ return {
 			-- C-k: Toggle signature help
 			--
 			-- See the full "keymap" documentation for information on defining your own keymap.
-			keymap = { preset = "default" },
+			keymap = { preset = "enter" },
 
 			appearance = {
 				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
