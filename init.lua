f6524e4851e6d73893907e881cbaad2500dbe5a3
Tim0305 <tabtim1015@gmail.com>
2025-04-10 15:11:38 -0600

    Feat: No lazy plugin updates popup

--- init.lua
+++ init.lua
@@ -4,3 +4,11 @@ require("win32yank")
 require("vim-config")
 -- Lazy.nvim
 require("config.lazy")
+
+-- Lazy Autoupdate
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   group = vim.api.nvim_create_augroup("LazyUpdate", { clear = true }),
+--   callback = function()
+--     require("lazy").update() -- Usar el método adecuado según la configuración de lazy.nvim
+--   end,
+-- })
